05dbf45e0477069f458a45e477c31c32538e8b15
badal-06 <badalpanwar91@gmail.com>
2025-08-26 23:58:43 +0530

    Add Create Array with Zeros, Ones, and Identity Matrix

---
+++ Create Array with Zeros, Ones, and Identity Matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+zeros_arr = np.zeros((3, 3))
+ones_arr = np.ones((2, 4))
+identity_arr = np.eye(4)
+
+print("Zeros Array:\n", zeros_arr)
+print("\nOnes Array:\n", ones_arr)
+print("\nIdentity Matrix:\n", identity_arr)
