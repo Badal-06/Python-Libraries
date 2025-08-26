c1ef48dd5e95cba3ab6e6dd8f3f7229af17e7d6d
badal-06 <badalpanwar91@gmail.com>
2025-08-26 23:55:38 +0530

    Add Basic Statistics with NumPy

---
+++ Basic Statistics with NumPy.py
@@ -0,0 +1,11 @@
+# NumPy: Basic statistics
+import numpy as np
+
+data = np.array([12, 15, 20, 18, 22, 27, 30])
+
+print("Data:", data)
+print("Mean:", np.mean(data))
+print("Median:", np.median(data))
+print("Standard Deviation:", np.std(data))
+print("Minimum:", np.min(data))
+print("Maximum:", np.max(data))
